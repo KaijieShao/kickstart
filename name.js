949ce797966567c8870867d93d2a4a511ec57e0c
Kaijie Shao <48752686+KaijieShao@users.noreply.github.com>
2025-07-17 10:51:21 -0600

    Create name.js

---
+++ name.js
@@ -0,0 +1,8 @@
+/**
+ * Greets a user by name.
+ * @param name - The name of the user to greet
+ * @returns A greeting string
+ */
+export function greet(name: string): string {
+  return `Hello, ${name}! Welcome to the project.`;
+}
